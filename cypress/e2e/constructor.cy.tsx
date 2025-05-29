d2445158c781d2f1f940fcb9fa3626378990ea15
Anastasia Grebennikova <anastasiagrebennik0va@yandex.ru>
2025-05-29 21:20:13 +0300

    fix after first review

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -12,13 +12,24 @@ describe('Страница конструктора', () => {
 
     describe('Сборка бургера', () => {
       it('Должен добавлять ингридиенты в конструктор при клике на кнопку добавления ингредиента', () => {
+        // Проверяем что ингредиентов нет в конструкторе перед добавлением
+        cy.contains('Оформить заказ')
+          .parent()
+          .parent('section')
+          .within(() => {
+            cy.contains('Биокотлета из марсианской Магнолии').should('not.exist');
+            cy.contains('Краторная булка N-200i').should('not.exist');
+          });
+
+        // Добавляем ингредиенты
         cy.contains('Биокотлета из марсианской Магнолии')
           .parent()
           .find('button')
-          .click({ force: true });
+          .click();
 
-        cy.contains('Краторная булка N-200i').parent().find('button').click({ force: true });
+        cy.contains('Краторная булка N-200i').parent().find('button').click();
 
+        // Проверяем что ингредиенты появились в конструкторе
         cy.contains('Оформить заказ')
           .parent()
           .parent('section')
@@ -30,22 +41,29 @@ describe('Страница конструктора', () => {
       });
 
       it('Должен удалять ингридиенты из конструктора при клике на кнопку удаления ингредиента', () => {
+        // Добавляем ингредиенты
         cy.contains('Биокотлета из марсианской Магнолии')
           .parent()
           .find('button')
-          .click({ force: true });
+          .click();
 
-        cy.contains('Краторная булка N-200i').parent().find('button').click({ force: true });
+        cy.contains('Краторная булка N-200i').parent().find('button').click();
 
+        // Проверяем что ингредиенты есть в конструкторе
         cy.contains('Оформить заказ')
           .parent()
           .parent('section')
           .within(() => {
+            cy.contains('Биокотлета из марсианской Магнолии').should('exist');
+            
+            // Удаляем ингредиент
             cy.contains('Биокотлета из марсианской Магнолии')
               .parent()
               .find('.constructor-element__action')
-              .click({ force: true });
+              .click();
 
+            // Проверяем что ингредиент удалился
+            cy.contains('Биокотлета из марсианской Магнолии').should('not.exist');
             cy.contains('2510').should('exist');
           });
       });
@@ -53,7 +71,7 @@ describe('Страница конструктора', () => {
 
     describe('Модальное окно ингредиента', () => {
       beforeEach(() => {
-        cy.contains('Биокотлета из марсианской Магнолии').parent().click({ force: true });
+        cy.contains('Биокотлета из марсианской Магнолии').parent().click();
       });
 
       it('Должен открывать модальное окно при клике на карточку ингридиента', () => {
@@ -63,13 +81,13 @@ describe('Страница конструктора', () => {
       });
 
       it('Должен закрывать модальное окно при клике на крестик', () => {
-        cy.get('#modals').find('button').click({ force: true });
+        cy.get('#modals').find('button').click();
 
         cy.get('#modals').should('not.contain.html');
       });
 
       it('Должен закрывать модальное окно при клике вне окна', () => {
-        cy.get('#modals').parent().click('topRight', { force: true });
+        cy.get('#modals').parent().click('topRight');
       });
     });
   });
@@ -106,25 +124,34 @@ describe('Страница конструктора', () => {
         delay: 100
       }).as('postOrder');
 
-      cy.contains('Биокотлета из марсианской Магнолии')
+      // Проверяем что конструктор пустой
+      cy.contains('Оформить заказ').parent().contains('0').should('exist');
+
+      // Добавляем ингредиенты
+      cy.contains('Филе Люминесцентного тетраодонтимформа')
         .parent()
         .find('button')
-        .click({ force: true });
+        .click();
 
-      cy.contains('Краторная булка N-200i').parent().find('button').click({ force: true });
+      cy.contains('Соус фирменный Space Sauce')
+        .parent()
+        .find('button')
+        .click();
 
-      cy.contains('Оформить заказ').click({ force: true });
+      cy.contains('Краторная булка N-200i').parent().find('button').click();
+
+      cy.contains('Оформить заказ').click();
 
       cy.contains('Оформляем заказ...').should('exist');
 
       cy.wait('@postOrder').then(() => {
-        cy.get('#modals').contains('63217').should('exist');
-        cy.get('#modals').parent().click('topRight', { force: true });
+        cy.get('#modals').contains('79054').should('exist');
+        cy.get('#modals').parent().click('topRight');
         cy.get('#modals').should('not.contain.html');
 
+        // Проверяем что конструктор очистился после заказа
         cy.contains('Выберите булки').should('exist');
         cy.contains('Выберите начинку').should('exist');
-
         cy.contains('Оформить заказ').parent().contains('0').should('exist');
       });
     });
